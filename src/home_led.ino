5b9ce08704041d16c3cd82885314f81589b32dac
Thomas Chandler <thomasch@ndler.ca>
2019-11-13 19:22:22 -0500

    Fix whitespace

--- src/home_led.ino
+++ src/home_led.ino
@@ -4,49 +4,35 @@
 // PSU_ON is active low
 #define PSU_ON 5
 
-        CRGB leds[NUM_LEDS];
-        void setup() {
-          FastLED.addLeds<WS2812B, 2, GRB>(leds, NUM_LEDS);
-          for (int i = 0; i < NUM_LEDS; i++) {
-                leds[i] = CRGB::Black; FastLED.show(); //delay(50); 
-                //leds[0] = CRGB::Black; FastLED.show(); delay(30);
-                 // ok; might get hot after a while, not tested
-                //LEDS.setBrightness(50); // Too bright; hot reg
-                //LEDS.setBrightness(100); // Too bright; regulator on arduino smells
-          }
-          FastLED.show();
-          pinMode(PSU_ON, OUTPUT);
-          digitalWrite(PSU_ON, LOW);
-          // Let PSU voltage settle - otherwise latter LEDs might not get set correctly and will latch
-          delay(500);
-          
-          for (int i = 0; i < NUM_LEDS; i++) {
-                //if (i>25) {
-                leds[i] = CRGB::Purple; FastLED.show(); delay(50);
-                LEDS.setBrightness(100);
-                //}
-                //leds[0] = CRGB::Black; FastLED.show(); delay(30);
-                 // ok; might get hot after a while, not tested
-                //LEDS.setBrightness(50); // Too bright; hot reg
-                //LEDS.setBrightness(100); // Too bright; regulator on arduino smells
-          }
-          FastLED.show();
-          // Power down after 10s - until remote control is setup
-          delay(10000);
-          digitalWrite(PSU_ON, HIGH);
-        }
-        void loop() { 
+// Started from https://github.com/FastLED/FastLED/wiki/Overview#quick-examples
 
+CRGB leds[NUM_LEDS];
+void setup() {
+  FastLED.addLeds<WS2812B, 2, GRB>(leds, NUM_LEDS);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black; FastLED.show();
+  }
+  FastLED.show();
+  pinMode(PSU_ON, OUTPUT);
+  digitalWrite(PSU_ON, LOW);
+  // Let PSU voltage settle - otherwise latter LEDs might not get set correctly and will latch
+  delay(500);
 
-          for (int i = 0; i < NUM_LEDS; i++) {
-                //if (i>25) {
-                leds[i] = CRGB::Purple; FastLED.show(); //delay(50);
-                LEDS.setBrightness(100);
-                //}
-                //leds[0] = CRGB::Black; FastLED.show(); delay(30);
-                 // ok; might get hot after a while, not tested
-                //LEDS.setBrightness(50); // Too bright; hot reg
-                //LEDS.setBrightness(100); // Too bright; regulator on arduino smells
-          }
-          FastLED.show();
-        }
+  LEDS.setBrightness(100);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Purple; FastLED.show();
+    delay(50);
+  }
+  FastLED.show();
+  // Power down after 10s - until remote control is setup
+  delay(10000);
+  digitalWrite(PSU_ON, HIGH);
+}
+
+void loop() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Purple; FastLED.show();
+    LEDS.setBrightness(100);
+  }
+  FastLED.show();
+}
